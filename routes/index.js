b09cb1788946d5243ca5da850bac65f267ea0d65
Ethan Raymond <ethan.raymond14@gmail.com>
2016-06-12 14:22:06 -0700

    Revert "updated readme and put keys in private file"
    This reverts commit f57adcc4e45bcd38c39ba3dd39771d5d33cab915.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var app = express();
 var router = express.Router();
 var subscribe = require('./subscribe');
 var bodyParser = require('body-parser');
+var mcapi = require('mailchimp-api');
 
+mc = new mcapi.Mailchimp('d63c6dbc225c25408c17b575d9e8f000-us13');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,9 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('css'));
 
+//router.post('/subscribe', function(req, res, next) {
+//  res.send('hi');
+//  console.log(req.body);
+//
+//});
+//router.post('/subscribe',subscribe.subscribe);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname,"..","index.html"));
 });
+//router.post('/subscribe',subscribe.subscribe);
 
 module.exports = router;
